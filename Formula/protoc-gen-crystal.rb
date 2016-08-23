6f3f8bddfe32030b1862f7c02b951a6cf5be2fa8
Jerome Gravel-Niquet <jeromegn@gmail.com>
2016-08-23 06:58:16 -0600

    update protoc-gen-crystal formula to 2.0.1

--- Formula/protoc-gen-crystal.rb
+++ Formula/protoc-gen-crystal.rb
@@ -1,9 +1,9 @@
 class ProtocGenCrystal < Formula
   desc "Protobuf generation for Crystal"
   homepage "https://github.com/jeromegn/protobuf.cr"
-  url "https://github.com/jeromegn/protobuf.cr/releases/download/v2.0.0/protoc-gen-crystal-darwin-x86_64.tar.gz"
-  version "2.0.0"
-  sha256 "aa791ec0c1562747161ad44ac5d057ac255b19b7cf4b60770efd840895a6c257"
+  url "https://github.com/jeromegn/protobuf.cr/releases/download/v2.0.1/protoc-gen-crystal-darwin-x86_64.tar.gz"
+  version "2.0.1"
+  sha256 "def7bb1ca66fbeee47c8badb665755734bf4f63bfecb7fd35a476508bfa9f7ab"
 
   def install
     bin.install 'protoc-gen-crystal'
